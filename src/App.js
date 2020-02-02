e34e5514650b839babe5af6076a3d158361592dc
willmorim <pdbawilliam@gmail.com>
2020-02-02 15:58:31 -0300

    Hook useMemo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App() {
-const [techs, setTechs] = useState([])
-const [newTech, setNewTechs] = useState('')
+  const [techs, setTechs] = useState([])
+  const [newTech, setNewTechs] = useState('')
 
-function handleAdd() {
-  setTechs([...techs, newTech])
-  setNewTechs('')
-}
-
-//componentDidMount
-useEffect(() => {
-  const storageTechs = localStorage.getItem('techs')
-
-  if(storageTechs){
-    setTechs(JSON.parse(storageTechs))
+  function handleAdd() {
+    setTechs([...techs, newTech])
+    setNewTechs('')
   }
-}, [])
 
-//componentDidUpdate
-useEffect(() => {
-  localStorage.setItem('techs', JSON.stringify(techs))
-}, [techs])
+  //componentDidMount
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs')
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs))
+    }
+  }, [])
+
+  //componentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs))
+  }, [techs])
+
+const techSize = useMemo(() => techs.length, [techs])
 
   return (
     <>
       <ul>
         {techs.map(tech => <li key={tech}>{tech}</li>)}
       </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
       <input value={newTech} onChange={e => setNewTechs(e.target.value)}></input>
       <button type="button" onClick={handleAdd}>Adicionar</button>
-   </>
+    </>
   );
 }
 
